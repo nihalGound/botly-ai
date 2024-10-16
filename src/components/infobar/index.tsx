ec5e75658efaeab4f3de86b140239e90863eeaa2
nihalGound <nsnihalgound123@gmail.com>
2024-10-16 16:29:50 +0530

    ready for deployment

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -10,23 +10,6 @@ const InfoBar = (props: Props) => {
   return (
     <div className=" flex w-full justify-between items-center py-1 mb-8 gap-y-2 max-md:flex-wrap">
         <BreadCrumb  />
-        <div className="flex gap-3 items-center  lg:pr-2">
-            <div>
-                <Card className="rounded-xl flex gap-3 py-3 px-4 text-ghost">
-                    <Trash />
-                    <Star></Star>
-                </Card>
-            </div>
-            <Avatar>
-                <AvatarFallback className="bg-orange text-white">
-                    <Headphones />
-                </AvatarFallback>
-            </Avatar>
-            <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-        </div>
     </div>
   )
 }
